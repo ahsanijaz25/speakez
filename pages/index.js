ebf7f58248e2c30438a71340b2b39266799df809
Ahsan Ijaz <ahsan.ijaz25@gmail.com>
2023-10-08 13:01:25 +0500

    first dep

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,270 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <header className={styles.header}>
+          <div className="container-lg">
+            <div className="d-flex">
+              <div  className="d-flex align-items-center">
+                <button className={styles.button}>
+                    <a href="#">
+                        Try Now 
+                        <span>
+                          <img width="40" src="/power-svgrepo-com.svg" alt="" />
+                        </span>
+                    </a>
+                </button>
+              </div>
+             
+              <div className={styles.logoContainer}>
+                  <div>
+                    <img src="/logo.png" alt="" />
+                  </div>
+              </div>
+            </div>
+          </div>
+      </header>
+      <section className={styles.homepage1}>
+          <div className="container-lg">
+              <div className="row mb-4">
+                <div className="col-lg-8">
+                    <div className={styles.homepage1inner}>
+                      <h1>A <span>new way</span>  to learn to <span>speak</span></h1>
+                      <p>
+                      This is a new way to communicate and get better at speaking. This is a new way to communicate and get better at speaking. This is a new way to communicate and get better at speaking. This is a new way to communicate and get better at speaking. This is a new way to communicate and get better at speaking.
+                      </p>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                  <div className={styles.hompage1tiles}>
+                      <div className={styles.hompage1tile}>
+                          <div className={styles.hompage1tileImg}>
+                            <img src="/hometick.png" alt="" />
+                          </div>
+                          <div className={styles.hompage1tiletxt}>
+                            <span>Accredited</span> by Speech Therapists
+                          </div>
+                      </div>
+                      <div className={styles.hompage1tile}>
+                          <div className={styles.hompage1tileImg}>
+                            <img src="/homedollar.png" alt="" />
+                          </div>
+                          <div className={styles.hompage1tiletxt}>
+                            <span>Free </span> of charge
+                          </div>
+                      </div>
+                      <div className={styles.hompage1tile}>
+                          <div className={styles.hompage1tileImg}>
+                            <img src="/hometime.png" alt="" />
+                          </div>
+                          <div className={styles.hompage1tiletxt}>
+                            <span> &lt;30 sec </span> for results
+                          </div>
+                      </div>
+                  </div>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                </div>
+              </div>
+              <div className={styles.sectionFooter}>
+                <div className={styles.socialicons}>
+                    <ul>
+                      <li><a href="#"><img src="/discord.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/message.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/twitter.png" alt="" /></a></li>
+                    </ul>
+                </div>
+                <div className={styles.arrowContainer}>
+                  <div className={styles.arrow}>
+                    <img src="/arrow.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              
+          </div>
+      </section>
+      <section className={styles.homepage2}>
+          <div className="container-lg">
+              <div className="row mb-4">
+                <div className="col-lg-12">
+                   <h2>The Problem</h2>
+                   <p className={styles.home2mainp}>6-8 million people in the US have speech impairment</p>
+                </div>
+                <div className="col-lg-12">
+                <div className={styles.hompage2tiles}>
+                      <div className={styles.hompage2tile}>
+                          <div className={styles.hompage2tileImg}>
+                            <img src="/setting.png" alt="" />
+                          </div>
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>Little Resources</h3>
+                            <p>Little to no <strong> online help apps</strong> for speech</p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          <div className={styles.hompage2tileImg}>
+                            <img src="/home2dollar.png" alt="" />
+                          </div>
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>Expensive</h3>
+                            <p>Speech apps cost up to  with little impact VS. speech therapists cost <strong> $30/mo</strong>  <strong> for speech</strong> </p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          <div className={styles.hompage2tileImg}>
+                            <img src="/microscope.png" alt="" />
+                          </div>
+                          <div className={styles.hompage2tiletxt}>
+                          <h3>Little Research</h3>
+                            <p>Speech impairments largely  <strong>under-researched</strong></p>
+                          
+                          </div>
+                      </div>
+                  </div>
+                  
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                </div>
+              </div>
+              <div className={styles.sectionFooter}>
+                <div className={styles.socialicons}>
+                    <ul>
+                      <li><a href="#"><img src="/discord.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/message.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/twitter.png" alt="" /></a></li>
+                    </ul>
+                </div>
+                <div className={styles.arrowContainer}>
+                  <div className={styles.arrow}>
+                    <img src="/arrow.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              
+          </div>
+      </section>
+      <section className={styles.homepage2 + ' ' + styles.homepage3}>
+          <div className="container-lg">
+              <div className="row mb-4">
+                <div className="col-lg-12">
+                   <h2> <span>Unlike any </span> other speaking tool</h2>
+                   <p className={styles.home2mainp}> <span>SpeakEZ</span> is the first and  <span>only browser</span> supported AI app that effectively tracks stutters and other speech disfluences. It’s your <span> personal speech therapist</span></p>
+                </div>
+                <div className="col-lg-12">
+                <div className={styles.hompage2tiles + ' mb-2'}>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>9+</h3>
+                            <p>Browsers</p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>0.1<span>sec</span></h3>
+                            <p>Latency</p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                          <h3>$0</h3>
+                            <p>Price</p>
+                          
+                          </div>
+                      </div>
+                  </div>
+                  <div className={styles.hompage2tiles}>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>5</h3>
+                            <p>Stutter Types Identification</p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                            <h3>100<span>%</span></h3>
+                            <p>Real-Time</p>
+                          </div>
+                      </div>
+                      <div className={styles.hompage2tile}>
+                          
+                          <div className={styles.hompage2tiletxt}>
+                          <h3>95<span>%</span></h3>
+                            <p>Hard-Onset</p>
+                          
+                          </div>
+                      </div>
+                  </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                </div>
+              </div>
+              <div className={styles.sectionFooter}>
+                <div className={styles.socialicons}>
+                    <ul>
+                      <li><a href="#"><img src="/discord.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/message.png" alt="" /></a></li>
+                      <li><a href="#"><img src="/twitter.png" alt="" /></a></li>
+                    </ul>
+                </div>
+                <div className={styles.arrowContainer}>
+                  <div className={styles.arrow}>
+                    <img src="/arrow.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              
+          </div>
+      </section>
+      <section className={styles.homepage4}>
+          <div className="container-lg">
+             <div className="row">
+               <div className="col-lg-4">
+                  <div className={styles.records}>
+                    <ul>
+                      <li>Availability</li>
+                      <li>Latency</li>
+                      <li>Price</li>
+                      <li>Stutters</li>
+                      <li>Hard-Onset</li>
+                    </ul>
+                  </div>
+               </div>
+               <div className="col-lg-4">
+                  <div className={styles.records}>
+                    <ul>
+                      <li>Availability</li>
+                      <li>Latency</li>
+                      <li>Price</li>
+                      <li>Stutters</li>
+                      <li>Hard-Onset</li>
+                    </ul>
+                  </div>
+               </div>
+               <div className="col-lg-4">
+               <div className={styles.records}>
+                    <ul>
+                      <li>Availability</li>
+                      <li>Latency</li>
+                      <li>Price</li>
+                      <li>Stutters</li>
+                      <li>Hard-Onset</li>
+                    </ul>
+                  </div>
+               </div>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+             </div>
+              
+          </div>
+      </section>
+    </>
+    
   );
 }
