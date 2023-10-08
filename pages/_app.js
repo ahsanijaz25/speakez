3420d57a0cf9f226b58b7677e4cfdd27353b2122
Ahsan Ijaz <ahsan.ijaz25@gmail.com>
2023-10-08 13:56:26 +0500

    rd commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Script from "next/script";
 import Head from "next/head";
-import "../global.css";export default function MyApp({ Component, pageProps }) {return (
+import "../styles/global.css";export default function MyApp({ Component, pageProps }) {return (
 <> 
      <Head> 
         <link    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
